aa87e5a81e1c57df65ee891aea3a03d580d0ad69
philemonheng <phil.hsy@outlook.com>
2020-09-26 20:07:38 +0800

    add title and components

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Home() {
   return (
     <div id="home-container">
-      Hello World; 
+      <h1> Centaur React API App </h1>
+
+      <div>
+        <h2> Ethereum Address: </h2>
+      </div>
+
+      <div>
+        <h2> Current Ethereum block height: </h2>
+      </div>
+
+      <div>
+        <h2> Current Ethereum block height: </h2>
+      </div>
     </div>
   );
 }
