0f88c0192ad623546ffd2a7a75d6e3a34693fa4e
philemonheng <phil.hsy@outlook.com>
2020-09-27 03:45:51 +0800

    add API calls

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './style.css';
 import Home from './Home';
 
 function App() {
+
   return (
     <div>
       <Home />
     </div>
   );
+
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// Using https://www.blockcypher.com/dev/ethereum/#address-api
 
 function Home() {
+  const [addBal, setAddBal] = useState(null);
+  const [blkHeight, setBlkHeight] = useState(null);
+  const blkAddress = "0xaedd8bc2c9abc2f7887ff4bbb33ca00874db05a5";
+
+  useState (async () => {
+    const responseBal = await fetch("https://api.blockcypher.com/v1/eth/main/addrs/" + blkAddress + "/balance");
+    const dataBal = await responseBal.json();
+    const addBal = dataBal.balance;
+    setAddBal(addBal);
+
+    const responseBlkHeight = await fetch("https://api.blockcypher.com/v1/eth/main");
+    const dataBlkHeight = await responseBlkHeight.json();
+    const blkHeight = dataBlkHeight.height;
+    setBlkHeight(blkHeight);
+  },[]);
+
   return (
     <div id="home-container">
-      <h1> Centaur React API App </h1>
+    <h1> Centaur React API App </h1>
 
-      <div>
-        <p> Ethereum Address: </p>
-      </div>
+    <div id="container">
+      <h3> Ethereum Address: </h3>
+      <p> {blkAddress} </p>
+    </div>
 
-      <div>
-        <p> Current Ethereum block height: </p>
-      </div>
+    <div id="container">
+      <h3> Current Ethereum block height: </h3>
+      <p> {blkHeight} </p>
+    </div>
 
-      <div>
-        <p> Current Ethereum block height: </p>
-      </div>
+    <div id="container">
+      <h3> Ethereum balance of address (wei): </h3>
+      <p> {addBal} </p>
+    </div>
+
     </div>
   );
 }
